695ea219a0943335e5b2bfc2007950c0b860610c
Ирина Баранова <irinabaranova@MacBook-Air-Irina.local>
2023-08-31 14:40:09 +0200

    settings project

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    watchForFileChanges: false,
+    defaultCommandTimeout: 30000,
+    pageLoadTimeout: 70000,
+    baseUrl: "https://kusto-gamma.vercel.app",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
